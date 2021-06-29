7ca1ca2f40e26f76430af47aa5255996dda39e2d
bohdanloik <bohdanloik@gmail.com>
2021-06-29 16:28:09 +0300

    - add component AddItemForm -

--- src/AddItemForm.tsx
+++ src/AddItemForm.tsx
@@ -1,24 +1,41 @@
 import React, {ChangeEvent, KeyboardEvent, useState} from 'react';
 
-export type AddItemFormPropsType = { 
-        title: string
-        onChangeHandler: (e: ChangeEvent<HTMLInputElement>) => void
-        onKeyPressHandler: (e: KeyboardEvent<HTMLInputElement>) => void
-        addTask: () => void
-        error: string | null
-
+type AddItemFormPropsType = { 
+        addItem: (title: string) => void
 }
 
 export const AddItemForm = (props: AddItemFormPropsType) => {
+    let [title, setTitle] = useState("")
+    let [error, setError] = useState<string | null>(null)
+
+    const onChangeHandler = (e: ChangeEvent<HTMLInputElement>) => {
+        setTitle(e.currentTarget.value);
+    }
+
+    const addItem = () => {
+        if (title.trim() !== '') {
+            props.addItem(title);
+            setTitle("");
+        } else {
+            setError('Title is required');
+        }
+    }
+
+    const onKeyPressHandler = (e: KeyboardEvent<HTMLInputElement>) => {
+        setError(null);  
+        if (e.charCode === 13) {
+            addItem();
+        }
+    }
 
     return(
         <div>       
-            <input value={props.title}
-                onChange={props.onChangeHandler}
-                onKeyPress={props.onKeyPressHandler}
+            <input value={title}
+                onChange={onChangeHandler}
+                onKeyPress={onKeyPressHandler}
             />
-            <button onClick={props.addTask}>+</button>
-            {props.error && <div className='error-message'>{props.error}</div>}
+            <button onClick={addItem}>+</button>
+            {error && <div className='error-message'>{error}</div>}
         </div>
     )
 }
